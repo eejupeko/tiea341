b10f110c0e296adc432224faee88bd638da9667c
eejupeko <eetu.j.p.korhonen@student.jyu.fi>
2014-09-17 07:40:17 +0300

    Tehtävän aloitus: basicSyntax.hs

---
+++ basicSyntax.hs
@@ -0,0 +1,15 @@
+testi = 
+	let
+		a = 5
+		b = head x
+		x = [7,2]
+	in a + b
+	
+testi2 = [a,b,c]
+	where
+		a = 5
+		b = a+c
+		c = testi
+		
+-- Molemmissa on helposti ymmärrettävä syntaksi ja määritelmä on
+-- mukavasti paloiteltu osiin ja siten voi olla helppolukuisempi
